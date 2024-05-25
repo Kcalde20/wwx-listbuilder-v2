ecdbd05e4595b9e8a2f42a07678e3636a4e76a15
Kevin Calderwood <kevincalderwood001@comcast.net>
2024-05-25 13:09:26 -0400

    moving list info up a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { ListService } from './shared/list.service';
+import { PosseService } from './shared/posse.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RouterModule],
+    imports: [RouterOutlet, RouterModule, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    title = 'wwx-listbuilder';
+    title = 'WWX-Listbuilder';
+
+    listService = inject(ListService);
+    posseService = inject(PosseService);
 }
